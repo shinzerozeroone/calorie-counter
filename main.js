12a200b8d0e3c57111a106cf934f60cf2662cacd
young.shark8484@gmail.com <young.shark8484@gmail.com>
2024-04-14 15:37:33 +0300

    added Entry

--- main.js
+++ main.js
@@ -11,3 +11,26 @@ function cleanInputString(str) {
   const regex = /[+-\s]/g;
   return str.replace(regex, "");
 }
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(
+    `#${entryDropdown.value} .input-container`
+  );
+
+  const entryNumber =
+    targetInputContainer.querySelectorAll('input[type="text"]').length;
+
+  const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type = "text" placeholder = "Name" id = "${entryDropdown.value}-${entryNumber}-name">
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">
+    `;
+
+  targetInputContainer.innerHTML += HTMLString;
+}
